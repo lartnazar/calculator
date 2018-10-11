cb7af4641d7e857c6947d8e393478ffa8027b1a6
lartnazar <lartnazar@yandex.ru>
2018-10-12 00:38:18 +0300

    add acceptance test

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 CALCULATOR_PORT=$(docker-compose	port	calculator	8080	|	cut	-d:	-f2)
-test $(curl efp7.sm-soft.ru:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
+test $(curl efp7.sm-soft.ru:8765/sum?a=1\&b=2) -eq 3
