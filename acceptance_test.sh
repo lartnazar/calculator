26a222394d627f949bb0e79eb07465ae81fa6047
lartnazar <lartnazar@yandex.ru>
2018-10-11 23:57:51 +0300

    fix port

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-test $(curl efp7.sm-soft.ru:8765/sum?a=1\&b=2) -eq 3
+CALCULATOR_PORT=$(docker-compose	port	calculator	8080	|	cut	-d:	-f2)
+test $(curl efp7.sm-soft.ru:$CALCULATOR_PORT/sum?a=1\&b=2) -eq 3
